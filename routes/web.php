826ce500d0ed8f327502fbfee10d17c854922191
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-08-01 21:24:31 +0600

    no init

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('react/index.html'));
+})->where('any', '.*');
+
