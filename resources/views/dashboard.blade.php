2d8687bcb8380ce4058ee48660d890dc4f6531d6
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-08-04 23:43:33 +0600

    nthg

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container text-center py-5">
-        <h1 class="display-4 text-black">Welcome Back!</h1>
+        <h1 class="display-4 text-primary">Welcome Back!</h1>
         <p class="lead text-secondary">Glad to see you again. Explore your dashboard and get started!</p>
     </div>
 @endsection
