c7ae931cd2eae15becce23f87a25f26434774d5d
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-09-08 20:53:33 +0600

    some

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ Route::post('/device-id',[FeatuerController::class,'addDeviceId']);
 Route::post('/login',[DeviceController::class,'login']);
 
 //Route::group(['middleware' => CheckDeviceId::SSC], function () {
-    Route::get('/SSC/SSC/PDF/{group}',[PDFController::class,'classGroup']);
-    Route::get('/SSC/SSC/Shortcut/{group}',[ShortcutController::class,'index']);
-    Route::get('/SSC/SSC/{id}',[PDFController::class,'classID']);
+    Route::get('/class/SSC/PDF/{group}',[PDFController::class,'classGroup']);
+    Route::get('/class/SSC/Shortcut/{group}',[ShortcutController::class,'index']);
+    Route::get('/class/SSC/{id}',[PDFController::class,'classID']);
 
     Route::get('/class/{class_name}/{types}',[ClassController::class,'index']);
     Route::get('/class/{id}',[ClassController::class,'show']);
