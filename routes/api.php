fe93be14e558965a5fb3c6db3898dbd9cfadc477
Md. Rubayet Islam <rubayetislam16@gmail.com>
2025-09-21 00:03:18 +0600

    some

--- routes/api.php
+++ routes/api.php
@@ -19,50 +19,31 @@ Route::post('/device-id',[FeatuerController::class,'addDeviceId']);
 
 Route::post('/login',[DeviceController::class,'login']);
 
-Route::get('/ssc/subjects',[ShortcutController::class,'subjects']);
+// ===== Specific Routes =====
 
-Route::get('/hsc/subjects',[ShortcutController::class,'hscsubjects']);
+// SSC routes
+Route::get('/ssc/subjects', [ShortcutController::class,'subjects']);
+Route::get('/class/ssc/pdf/{group}', [PDFController::class,'classGroup']);
+Route::get('/class/ssc/video/{group}', [PDFController::class,'videoclassGroup']);
+Route::get('/class/ssc/shortcut/{group}', [ShortcutController::class,'index']);
+Route::get('/class/ssc/shortcut/video/{group}', [ShortcutController::class,'videoindex']);
+Route::get('/class/ssc/allpdf/{group}/{question_types}/{subject_id}', [ShortcutController::class,'allPDF']);
+Route::get('/class/ssc/allpdf/video/{group}/{question_types}/{subject_id}', [ShortcutController::class,'videoallPDF']);
+Route::get('/class/ssc/video/{group}/{subject_id}', [ShortcutController::class,'videoCourse']);
+Route::get('/class/ssc/{id}', [PDFController::class,'classID']);
 
-Route::get('/honors/subjects',[ShortcutController::class,'honorssubjects']);
+// HSC routes
+Route::get('/hsc/subjects', [ShortcutController::class,'hscsubjects']);
+Route::get('/hsc/pdf/{group}/{hsc_year}', [PDFController::class,'hscpdf']);
+Route::get('/hsc/shortcut/{group}/{hsc_year}', [ShortcutController::class,'hscshortcut']);
+Route::get('/hsc/allpdf/{group}/{question_types}/{subject_id}/{hsc_year}', [ShortcutController::class,'hscallPDF']);
+Route::get('/hsc/class', [ShortcutController::class,'hscClassList']);
 
-Route::get('/hsc/class',[ShortcutController::class,'hscClassList']);
+// Honors routes
+Route::get('/honors/subjects', [ShortcutController::class,'honorssubjects']);
+Route::get('/class/honors/group-pdf/{group}', [PDFController::class,'honorsGroup']);
+Route::get('/class/honors/mcq/question/{group}/{subject_id}', [ShortcutController::class,'mcqQuestion']);
 
-Route::get('{class_name}/{check}',[ClassController::class,'rubayet']);
-
-
-    Route::get('/class/ssc/pdf/{group}',[PDFController::class,'classGroup']);
-
-    Route::get('/class/ssc/video/{group}',[PDFController::class,'videoclassGroup']);
-
-    Route::get('/class/honors/group-pdf/{group}',[PDFController::class,'honorsGroup']);
-
-    Route::get('/class/ssc/shortcut/{group}',[ShortcutController::class,'index']);
-
-    Route::get('/class/ssc/shortcut/video/{group}',[ShortcutController::class,'videoindex']);
-
-    Route::get('/class/ssc/allpdf/{group}/{question_types}/{subject_id}',[ShortcutController::class,'allPDF']);
-
-    Route::get('/class/ssc/allpdf/video/{group}/{question_types}/{subject_id}',[ShortcutController::class,'videoallPDF']);
-
-    Route::get('/class/honors/mcq/question/{group}/{subject_id}',[ShortcutController::class,'mcqQuestion']);
-
-    Route::get('/class/ssc/{id}',[PDFController::class,'classID']);
-
-
-
-
-    Route::get('/class/ssc/video/{group}/{subject_id}',[ShortcutController::class,'videoCourse']);
-
-
-
-    Route::get('/class/{id}',[ClassController::class,'show']);
-
-    Route::get('/hsc/pdf/{group}/{hsc_year}',[PDFController::class,'hscpdf']);
-
-    Route::get('/hsc/shortcut/{group}/{hsc_year}',[ShortcutController::class,'hscshortcut']);
-
-    Route::get('/hsc/allpdf/{group}/{question_types}/{subject_id}/{hsc_year}',[ShortcutController::class,'hscallPDF']);
-
-
-
-
+// ===== Generic Routes (Catch-all) =====
+Route::get('/class/{id}', [ClassController::class,'show']);
+Route::get('{class_name}/{check}', [ClassController::class,'rubayet']);
